6e7e39c8fd49fc094187d4e2ad2fc671e18ec6e6
Maycon Douglas <mayconclementino44@gmail.com>
2023-06-16 08:51:24 -0300

    webdriver

--- app.py
+++ app.py
@@ -30,20 +30,23 @@ option = Options()
 def iniciar_driver():
 
     chrome_options = Options()
-    chrome_options.binary_location = os.environ.get("/usr/bin/google-chrome")
-    arguments = ['--lang=pt-BR', '--window-size=1000,700', '--incognito', '--disable-gpu', '--no-sandbox', '--headless', '--disable-dev-shm-usage']
+
+    arguments = ['--lang=en-US', '--window-size=1920,1080',
+                 '--incognito', '--disable-gpu', '--no-sandbox', '--headless', '--disable-dev-shm-usage']
+
     for argument in arguments:
         chrome_options.add_argument(argument)
-
+    chrome_options.headless = True
     chrome_options.add_experimental_option('prefs', {
         'download.prompt_for_download': False,
         'profile.default_content_setting_values.notifications': 2,
-        'profile.default_content_setting_values.automatic_downloads': 1,
+        'profile.default_content_setting_values.automatic_downloads': 1
 
     })
 
-    driver = webdriver.Chrome(executable_path= os.environ.get("/auto/Lib/site-packages/webdriver_manager"), options=chrome_options)
-
+    driver = webdriver.Chrome(service=ChromeService(
+        ChromeDriverManager().install()), options=chrome_options)
+    
     return driver
 
 
