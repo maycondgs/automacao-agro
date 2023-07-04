844dbd38047f1d451139341ca14df4f5890f9ea2
Maycon Douglas <mayconclementino44@gmail.com>
2023-07-04 11:57:07 -0300

    update-versao2

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.select import Select
 import pandas as pd
 from time import sleep
 from bs4 import BeautifulSoup
