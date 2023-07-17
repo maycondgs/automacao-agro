991971313675a044ab9482eee83cb76677eaa4ad
Maycon Douglas <mayconclementino44@gmail.com>
2023-07-17 10:27:08 -0300

    update-vfinal71

--- app.py
+++ app.py
@@ -2308,12 +2308,12 @@ def scrapy_precos():
 def run(job):
     threaded = threading.Thread(target=job)
     threaded.start()
-    schedule.every(1).minute.do(run, scrapy_noticias)
-    schedule.every().day.at("01:00:00",'America/Sao_Paulo').do(run, scrapy_precos)
-    schedule.every().monday.do(run, scrapy_tabela)
 
 
-scrapy_precos()
+schedule.every(1).minute.do(run, scrapy_noticias)
+schedule.every().day.at("01:00",'America/Sao_Paulo').do(run, scrapy_precos)
+schedule.every().monday.do(run, scrapy_tabela)
+
 
 while 1:
     schedule.run_pending()
