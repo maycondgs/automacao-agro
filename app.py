0f95282eab75f5d791df4c4ee2aa6b5f6dd4b215
Maycon Douglas <mayconclementino44@gmail.com>
2023-07-19 09:06:16 -0300

    up-testev1

--- app.py
+++ app.py
@@ -2312,10 +2312,8 @@ def run(job):
     threaded.start()
     
 
+scrapy_precos()
 
-schedule.every(1).minute.do(run, scrapy_noticias)
-schedule.every().day.at("01:00",'America/Sao_Paulo').do(run, scrapy_precos)
-schedule.every().monday.do(run, scrapy_tabela)
 
 while 1:
     schedule.run_pending()
