7d5947d6d55d407f8832dc474d84f6f01624b12f
Maycon Douglas <mayconclementino44@gmail.com>
2024-03-14 04:59:32 -0300

    deploy-test-prices-10

--- crawler_prices_drct.py
+++ crawler_prices_drct.py
@@ -57,9 +57,8 @@ def iniciar_driver():
 
     })
     
-    service = Service()
 
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver = webdriver.Chrome('/usr/bin/chromedriver', options=chrome_options)
 
     wait = WebDriverWait(
         driver,
