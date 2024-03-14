926ce865c5243e483e0a1264ad9f24dcbedb79f4
Maycon Douglas <mayconclementino44@gmail.com>
2024-03-14 05:11:12 -0300

    deploy-test-prices-11

--- crawler_prices_drct.py
+++ crawler_prices_drct.py
@@ -49,7 +49,7 @@ def iniciar_driver():
     chrome_options = Options()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--incognito')
-    options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--no-sandbox')
 
 
     driver = webdriver.Chrome(service=service, options=chrome_options)
