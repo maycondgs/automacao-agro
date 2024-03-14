cc40bdea65fada4342a481be164ef52dec1d2953
Maycon Douglas <mayconclementino44@gmail.com>
2024-03-14 04:42:19 -0300

    deploy-test-prices-5

--- crawler_prices.py
+++ crawler_prices.py
@@ -44,9 +44,11 @@ db = mysql.connector.connect(
 
 def iniciar_driver():
     chrome_options = Options()
-    arguments = ['--lang=pt-BR', '--start-maximized', '--incognito', '--headless']
-    for argument in arguments:
-        chrome_options.add_argument(argument)
+
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--incognito')
+    chrome_options.add_argument('--start-maximized')
+
 
     chrome_options.add_experimental_option('prefs', {
         'download.prompt_for_download': False,
@@ -54,6 +56,7 @@ def iniciar_driver():
         'profile.default_content_setting_values.automatic_downloads': 1,
 
     })
+    
     service = Service()
     driver = webdriver.Chrome('/usr/bin/chromedriver', service=service, options=chrome_options)
 
--- crawler_prices_drct.py
+++ crawler_prices_drct.py
@@ -42,12 +42,13 @@ db = mysql.connector.connect(
 
 
 
-
 def iniciar_driver():
     chrome_options = Options()
-    arguments = ['--lang=pt-BR', '--start-maximized', '--incognito', '--headless']
-    for argument in arguments:
-        chrome_options.add_argument(argument)
+
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--incognito')
+    chrome_options.add_argument('--start-maximized')
+
 
     chrome_options.add_experimental_option('prefs', {
         'download.prompt_for_download': False,
@@ -55,6 +56,7 @@ def iniciar_driver():
         'profile.default_content_setting_values.automatic_downloads': 1,
 
     })
+    
     service = Service()
     driver = webdriver.Chrome('/usr/bin/chromedriver', service=service, options=chrome_options)
 
@@ -70,6 +72,7 @@ def iniciar_driver():
     )
 
     return driver,wait
+
 
 
 
