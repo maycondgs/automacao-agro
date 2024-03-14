f95b05c0f77fa61137dd8367d6b0121352100e62
Maycon Douglas <mayconclementino44@gmail.com>
2024-03-14 05:06:00 -0300

    deploy-test-prices-10

--- crawler_prices_drct.py
+++ crawler_prices_drct.py
@@ -47,16 +47,7 @@ def iniciar_driver():
 
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--incognito')
-    chrome_options.add_argument('--start-maximized')
 
-
-    chrome_options.add_experimental_option('prefs', {
-        'download.prompt_for_download': False,
-        'profile.default_content_setting_values.notifications': 2,
-        'profile.default_content_setting_values.automatic_downloads': 1,
-
-    })
-    
 
     driver = webdriver.Chrome('/usr/bin/google-chrome', options=chrome_options)
 
