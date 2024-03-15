317dd75dea19d4bc80ee4d6c442e2eaa06dfdbf8
Maycon Douglas <mayconclementino44@gmail.com>
2024-03-15 13:44:24 -0300

    update-browser-1

--- crawler_prices_drct.py
+++ crawler_prices_drct.py
@@ -51,6 +51,8 @@ def iniciar_driver():
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
 
+    chrome_options.binary_location = '/usr/bin/chromium-browser'
+
     service = Service('/usr/bin/chromedriver')
 
     driver = webdriver.Chrome(service=service, options=chrome_options)
