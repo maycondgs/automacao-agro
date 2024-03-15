18f784fddc2656009d214e675fd51ef5dc37f299
Maycon Douglas <mayconclementino44@gmail.com>
2024-03-15 15:01:36 -0300

    update-browser-3

--- firefox.py
+++ firefox.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
+import os
 
 # Configuração do driver (geckodriver)
 geckodriver_path = '/usr/bin/geckodriver'
-driver = webdriver.Firefox(executable_path=geckodriver_path)
+os.environ['PATH'] += os.pathsep + geckodriver_path
+
+driver = webdriver.Firefox()
 
 # Exemplo: abrindo o Google
 driver.get('https://www.google.com')
