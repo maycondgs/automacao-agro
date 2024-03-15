9f75525d5e02cf54303c2a3a0524355f697ce69e
Maycon Douglas <mayconclementino44@gmail.com>
2024-03-15 15:05:07 -0300

    update-browser-3

--- firefox.py
+++ firefox.py
@@ -7,7 +7,7 @@ geckodriver_path = '/usr/bin/geckodriver'
 os.environ['PATH'] += os.pathsep + geckodriver_path
 
 firefox_options = Options()
-firefox_options.add_argument('--headless')
+firefox_options.add_argument('-headless')
 
 driver = webdriver.Firefox()
 
