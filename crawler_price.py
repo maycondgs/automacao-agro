979d68e94dfb3a1e3faef000ff79f0b023899759
Maycon Douglas <mayconclementino44@gmail.com>
2024-03-15 14:15:02 -0300

    update-browser-2

--- crawler_price.py
+++ crawler_price.py
@@ -50,8 +50,7 @@ def iniciar_driver():
     chrome_options.add_argument('--incognito')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
-
-    chrome_options.binary_location = "/usr/bin/chromium-browser"
+    
 
     service = Service("/usr/bin/chromedriver")
 
