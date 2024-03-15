7c578ab3242c76522fe744fff06b3ff538834eba
Maycon Douglas <mayconclementino44@gmail.com>
2024-03-15 15:04:08 -0300

    update-browser-3

--- firefox.py
+++ firefox.py
@@ -1,9 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 import os
 
 # Configuração do driver (geckodriver)
 geckodriver_path = '/usr/bin/geckodriver'
 os.environ['PATH'] += os.pathsep + geckodriver_path
+
+firefox_options = Options()
+firefox_options.headless = True
 
 driver = webdriver.Firefox()
 
