6fe492f72c49a20f5e53822687c76a944aee58f0
Maycon Douglas <mayconclementino44@gmail.com>
2024-03-21 18:53:13 -0300

    update-pages-1

--- crawler_prices.py
+++ crawler_prices.py
@@ -57,7 +57,7 @@ def iniciar_driver():
     chrome_options.add_argument('--incognito')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    #chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--headless')
 
 
     service = Service()
@@ -80,8 +80,8 @@ def iniciar_driver():
 
 
 
-#pytesseract.pytesseract.tesseract_cmd = "/usr/bin/tesseract"
-pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
+pytesseract.pytesseract.tesseract_cmd = "/usr/bin/tesseract"
+#pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
 
 continu = True
 
