d62f65b54b9e1921371f0250d8ccb56e0eee8bf0
Maycon Douglas <mayconclementino44@gmail.com>
2024-03-22 06:41:43 -0300

    update-pages-2

--- crawler_prices_instant.py
+++ crawler_prices_instant.py
@@ -161,7 +161,8 @@ def busca(driver,wait, prodformat):
     sleep(5)
 
     try:
-        dattaa = wait.until(condicao_esperada.presence_of_element_located((By.XPATH,'/html/body/div[1]/main/div/div/div/div[1]/div[1]/div/div/div/form/div[2]/div[3]/div[2]/div/div[1]/div/input'))).click()
+        dattaa = wait.until(condicao_esperada.presence_of_element_located((By.XPATH,'/html/body/div[1]/main/div/div/div/div[1]/div[1]/div/div/div/form/div[2]/div[3]/div[2]/div/div[1]/div/input')))
+        driver.execute_script("arguments[0].click();", dattaa)
         sleep(3)
 
         btn_date = wait.until(condicao_esperada.element_to_be_clickable((By.XPATH,'//th[@class="today"]'))).click()
